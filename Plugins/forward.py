38376e61ad8e07c9899d32eac11020eb102fe2aa
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-20 12:48:51 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -27,9 +27,8 @@ def shorten_url(url):
 @channelforward.on_message(filters.channel)
 async def forward(client, message):
     try:
-        for id in Config.CHANNEL:
-            from_channel, to_channel = id.split(":")
-            if message.chat.id == int(from_channel):
+        for group_name, channels in Config.CHANNELS.items():
+            if message.chat.id in map(int, channels['sources']):
                 # Extract Terabox links using regex to handle various formats
                 text = message.caption or message.text or ""
                 terabox_links = re.findall(r'https://1024terabox.com/s/\S+', text)
@@ -44,16 +43,20 @@ async def forward(client, message):
 
                 # Send media thumbnail with formatted caption
                 if message.photo:
-                    await client.send_photo(int(to_channel), message.photo.file_id, caption=caption.strip())
+                    for destination in channels['destinations']:
+                        await client.send_photo(int(destination), message.photo.file_id, caption=caption.strip())
                 elif message.video:
-                    await client.send_video(int(to_channel), message.video.file_id, caption=caption.strip())
+                    for destination in channels['destinations']:
+                        await client.send_video(int(destination), message.video.file_id, caption=caption.strip())
                 elif message.document:
-                    await client.send_document(int(to_channel), message.document.file_id, caption=caption.strip())
+                    for destination in channels['destinations']:
+                        await client.send_document(int(destination), message.document.file_id, caption=caption.strip())
                 else:
                     # Send text message with only shortened Terabox links
-                    await client.send_message(int(to_channel), text=caption.strip())
+                    for destination in channels['destinations']:
+                        await client.send_message(int(destination), text=caption.strip())
 
-                logger.info(f"Forwarded a modified message with media and shortened Terabox links from {from_channel} to {to_channel}")
+                logger.info(f"Forwarded a modified message with media and shortened Terabox links from {group_name}")
                 await asyncio.sleep(1)
     except Exception as e:
         logger.exception(e)
