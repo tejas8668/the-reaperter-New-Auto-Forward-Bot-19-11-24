a1810b1897bd614ba65c623ab112ba65ab7ed4fe
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-20 10:28:08 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -18,20 +18,21 @@ async def forward(client, message):
                 terabox_links = re.findall(r'https://1024terabox.com/s/\S+', text)
 
                 # Format the caption with Terabox links only
-                caption = "\n".join(terabox_links)
+                if terabox_links:
+                    caption = "\n".join(terabox_links)
 
-                # Send media thumbnail with formatted caption
-                if message.photo:
-                    await client.send_photo(int(to_channel), photo=message.photo.file_id, caption=caption.strip())
-                elif message.video:
-                    await client.send_video(int(to_channel), video=message.video.file_id, caption=caption.strip())
-                elif message.document:
-                    await client.send_document(int(to_channel), document=message.document.file_id, caption=caption.strip())
-                else:
-                    # Send text message with only Terabox links
-                    await client.send_message(int(to_channel), text=caption.strip())
+                    # Send media thumbnail with formatted caption
+                    if message.photo:
+                        await client.send_photo(int(to_channel), message.photo.file_id, caption=caption.strip())
+                    elif message.video:
+                        await client.send_video(int(to_channel), message.video.file_id, caption=caption.strip())
+                    elif message.document:
+                        await client.send_document(int(to_channel), message.document.file_id, caption=caption.strip())
+                    else:
+                        # Send text message with only Terabox links
+                        await client.send_message(int(to_channel), text=caption.strip())
 
-                logger.info(f"Forwarded a modified message with media and Terabox links from {from_channel} to {to_channel}")
-                await asyncio.sleep(1)
+                    logger.info(f"Forwarded a modified message with media and Terabox links from {from_channel} to {to_channel}")
+                    await asyncio.sleep(1)
     except Exception as e:
         logger.exception(e)
