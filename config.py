30259fec8106b858c3d228018ca20e51f3a6b128
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-20 16:46:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ class Config(object):
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     CHANNELS = {
         "group_A": {
-            "sources": ["-1002301291753", "-1002294100731"],
+            "sources": ["-1002488212445"],
             "destinations": ["-1002328051849", "-1002387272968"]
         },
         "group_B": {
-            "sources": ["-1002448656249", "-1006666666666"],
-            "destinations": ["-1002280290177", "-1008888888888"]
+            "sources": ["-1002349374753"],
+            "destinations": ["-1002280290177", "-1002387272968"]
         },
         "group_C": {
-            "sources": ["-1002260493720", "-1001010101010"],
-            "destinations": ["-1002416263186", "-1001212121212"]
+            "sources": ["-1002377412867"],
+            "destinations": ["-1002416263186", "-1002387272968"]
         },
         "group_D": {
-            "sources": ["-1001313131313", "-1001414141414"],
-            "destinations": ["-1001515151515", "-1001616161616"]
+            "sources": ["-1002402818813"],
+            "destinations": ["-1002387272968", "-1002387272968"]
         }
     }  # Ensure this closing brace matches the opening brace for CHANNELS
